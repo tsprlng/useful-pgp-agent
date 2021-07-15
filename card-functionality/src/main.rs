32dbecbda2006a4b743e7080be414ae268dc381e
Heiko Schaefer <heiko@schaefer.name>
2021-07-15 13:13:56 +0200

    Test code for key uploading

--- card-functionality/src/main.rs
+++ card-functionality/src/main.rs
@@ -28,9 +28,14 @@
 use anyhow::Result;
 use std::collections::HashMap;
 
+use sequoia_openpgp::parse::Parse;
+use sequoia_openpgp::Cert;
+
 use openpgp_card::apdu::PcscClient;
 use openpgp_card::card_app::CardApp;
-use openpgp_card::{CardClientBox, Sex};
+use openpgp_card::{CardClientBox, KeyType, Sex};
+
+mod util;
 
 #[derive(Debug)]
 enum TestResult {
@@ -40,33 +45,81 @@ enum TestResult {
 
 type TestOutput = Vec<TestResult>;
 
-/// run after each "upload keys", if key *was* uploaded (?)
+/// Map: Card ident -> TestOutput
+type TestsOutput = HashMap<String, TestOutput>;
+
+/// Run after each "upload keys", if key *was* uploaded (?)
 fn test_decrypt() {
     // FIXME
     unimplemented!()
 }
 
-/// run after each "upload keys", if key *was* uploaded (?)
+/// Run after each "upload keys", if key *was* uploaded (?)
 fn test_sign() {
     // FIXME
     unimplemented!()
 }
 
-fn test_upload_keys_general() {
-    // FIXME
+fn check_key_upload_metadata(
+    ca: &mut CardApp,
+    meta: &[(String, u32)],
+) -> Result<()> {
+    let ard = ca.get_app_data()?;
 
-    // check fingerprint
-    // get_algorithm_attributes
+    // check fingerprints
+    let card_fp = CardApp::get_fingerprints(&ard)?;
+
+    let sig = card_fp.signature().expect("signature fingerprint");
+    assert_eq!(format!("{:X}", sig), meta[0].0);
+
+    let dec = card_fp.decryption().expect("decryption fingerprint");
+    assert_eq!(format!("{:X}", dec), meta[1].0);
+
+    let auth = card_fp
+        .authentication()
+        .expect("authentication fingerprint");
+    assert_eq!(format!("{:X}", auth), meta[2].0);
+
     // get_key_generation_times
+    let card_kg = CardApp::get_key_generation_times(&ard)?;
+
+    let sig: u32 =
+        card_kg.signature().expect("signature creation time").into();
+    assert_eq!(sig, meta[0].1);
+
+    let dec: u32 = card_kg
+        .decryption()
+        .expect("decryption creation time")
+        .into();
+    assert_eq!(dec, meta[1].1);
+
+    let auth: u32 = card_kg
+        .authentication()
+        .expect("authentication creation time")
+        .into();
+    assert_eq!(auth, meta[2].1);
+
+    Ok(())
 }
 
-fn test_upload_keys_rsa() {
+fn check_key_upload_algo_attrs() -> Result<()> {
+    // get_algorithm_attributes
     // FIXME
-    unimplemented!()
 
-    // upload key
+    Ok(())
+}
 
-    // test upload general - checks
+fn test_upload_keys_rsa_2k(ca: &mut CardApp) -> Result<TestOutput> {
+    let verify = ca.verify_pw3("12345678")?;
+    verify.check_ok()?;
+
+    let cert = Cert::from_file("data/rsa2k.sec")?;
+    let meta = util::upload_subkeys(ca, &cert)?;
+
+    check_key_upload_metadata(ca, &meta)?;
+    check_key_upload_algo_attrs()?;
+
+    Ok(vec![])
 }
 
 fn test_upload_keys_25519() {
@@ -85,6 +138,11 @@ fn test_keygen() {
     // FIXME
     // (implementation of this functionality is still missing in openpgp-card)
     unimplemented!()
+}
+
+fn test_reset(ca: &mut CardApp) -> Result<TestOutput> {
+    let res = ca.factory_reset()?;
+    Ok(vec![])
 }
 
 /// Sets name, lang, sex, url; then reads the fields from the card and
@@ -179,15 +237,21 @@ fn test_verify(ca: &mut CardApp) -> Result<TestOutput> {
 fn run_test(
     cards: &[&str],
     t: fn(&mut CardApp) -> Result<TestOutput>,
-) -> Result<HashMap<String, TestOutput>> {
+) -> Result<TestsOutput> {
     let mut out = HashMap::new();
 
     for card in PcscClient::list_cards()? {
         let card_client = Box::new(card) as CardClientBox;
+
         let mut ca = CardApp::new(card_client);
 
+        // Select OpenPGP applet
         let res = ca.select()?;
         res.check_ok()?;
+
+        // Set Card Capabilities (chaining, command length, ..)
+        let ard = ca.get_app_data()?;
+        ca = ca.init_caps(&ard)?;
 
         let ard = ca.get_app_data()?;
         let app_id = CardApp::get_aid(&ard)?;
@@ -196,7 +260,6 @@ fn run_test(
             println!("Running Test on {}:", app_id.ident());
 
             let res = t(&mut ca);
-            println!("{:x?}", res);
 
             out.insert(app_id.ident(), res?);
         }
@@ -206,15 +269,41 @@ fn run_test(
 }
 
 fn main() -> Result<()> {
+    env_logger::init();
+
     // list of card idents to runs the tests on
     let cards = vec![
-        "0006:16019180", // Yubikey 5
-        "0005:0000A835", // FLOSS Card 3.4
-        "FFFE:57183146", // Rysim Gnuk (green)
+        "0006:16019180", /* Yubikey 5 */
+        "0005:0000A835", /* FLOSS Card 3.4 */
+        "FFFE:57183146", /* Gnuk Rysim (green) */
+
+                         // "FFFE:4231EB6E", /* Gnuk FST */
     ];
 
-    let _verify_res = run_test(&cards, test_verify)?;
-    let _userdata_res = run_test(&cards, test_set_user_data)?;
+    // println!("reset");
+    // let _ = run_test(&cards, test_reset)?;
+    //
+    // println!("verify");
+    // let verify_out = run_test(&cards, test_verify)?;
+    // println!("{:x?}", verify_out);
+    //
+    // println!("set user data");
+    // let userdata_out = run_test(&cards, test_set_user_data)?;
+    // println!("{:x?}", userdata_out);
+
+    // upload RSA keys
+    println!("upload RSA2k key");
+    let upload_out = run_test(&cards, test_upload_keys_rsa_2k)?;
+    println!("{:x?}", upload_out);
+
+    // sign
+    // decrypt
+
+    // upload 25519 keys
+    // sign
+    // decrypt
+
+    // upload some key with pw
 
     Ok(())
 }
---
+++ card-functionality/src/util.rs
@@ -0,0 +1,72 @@
+use anyhow::{anyhow, Result};
+
+use sequoia_openpgp::cert::amalgamation::key::ValidKeyAmalgamation;
+use sequoia_openpgp::packet::key::{SecretParts, UnspecifiedRole};
+use sequoia_openpgp::policy::StandardPolicy;
+use sequoia_openpgp::Cert;
+
+use openpgp_card::card_app::CardApp;
+use openpgp_card::KeyType;
+use openpgp_card_sequoia::vka_as_uploadable_key;
+use std::time::SystemTime;
+
+pub const SP: &StandardPolicy = &StandardPolicy::new();
+
+pub(crate) fn upload_subkeys(
+    ca: &mut CardApp,
+    cert: &Cert,
+) -> Result<Vec<(String, u32)>> {
+    let mut out = vec![];
+    let mut gentime = 0;
+
+    for kt in [
+        KeyType::Signing,
+        KeyType::Decryption,
+        KeyType::Authentication,
+    ] {
+        let vka = get_subkey(cert, kt)?;
+
+        // store fingerprint as return-value
+        let fp = vka.fingerprint().to_hex();
+        // store key creation time as return-value
+        let creation = vka
+            .creation_time()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as u32;
+
+        out.push((fp, creation));
+
+        // upload key
+        let cuk = vka_as_uploadable_key(vka, None);
+        let res = ca.upload_key(cuk, kt)?;
+    }
+
+    Ok(out)
+}
+
+fn get_subkey(
+    cert: &Cert,
+    key_type: KeyType,
+) -> Result<ValidKeyAmalgamation<'_, SecretParts, UnspecifiedRole, bool>> {
+    // Find all suitable (sub)keys for key_type.
+    let mut valid_ka = cert
+        .keys()
+        .with_policy(SP, None)
+        .secret()
+        .alive()
+        .revoked(false);
+    valid_ka = match key_type {
+        KeyType::Decryption => valid_ka.for_storage_encryption(),
+        KeyType::Signing => valid_ka.for_signing(),
+        KeyType::Authentication => valid_ka.for_authentication(),
+        _ => return Err(anyhow!("Unexpected KeyType")),
+    };
+
+    // FIXME: for now, we just pick the first (sub)key from the list
+    if let Some(vka) = valid_ka.next() {
+        Ok(vka)
+    } else {
+        Err(anyhow!("No suitable (sub)key found"))
+    }
+}
