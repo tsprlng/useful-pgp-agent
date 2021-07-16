610478695f8f388d5a383397ba29f41977d3917e
Heiko Schaefer <heiko@schaefer.name>
2021-07-16 16:46:34 +0200

    Bugfix in interpreting Sex value

--- openpgp-card/src/lib.rs
+++ openpgp-card/src/lib.rs
@@ -167,9 +167,9 @@ impl Sex {
 impl From<u8> for Sex {
     fn from(s: u8) -> Self {
         match s {
-            31 => Sex::Male,
-            32 => Sex::Female,
-            39 => Sex::NotApplicable,
+            0x31 => Sex::Male,
+            0x32 => Sex::Female,
+            0x39 => Sex::NotApplicable,
             _ => Sex::NotKnown,
         }
     }
