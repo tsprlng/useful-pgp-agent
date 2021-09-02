18819c65d9ff0a022706ad0028cfe3e72b552123
Heiko Schaefer <heiko@schaefer.name>
2021-09-02 16:52:47 +0200

    Edit comment

--- openpgp-card/src/lib.rs
+++ openpgp-card/src/lib.rs
@@ -107,7 +107,7 @@ impl CardCaps {
     }
 }
 
-/// Enum to identify the Key-slots on an OpenPGP card
+/// Identify a Key slot on an OpenPGP card
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 #[non_exhaustive]
 pub enum KeyType {
