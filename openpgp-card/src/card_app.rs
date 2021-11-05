3d821e2c5f021acd158444c687002ca96705e55d
Heiko Schaefer <heiko@schaefer.name>
2021-11-05 13:32:39 +0100

    Add internal_authenticate() to the public API.

--- openpgp-card/src/card_app.rs
+++ openpgp-card/src/card_app.rs
@@ -522,7 +522,7 @@ impl CardApp {
 
     /// Run signing operation on the smartcard (low level operation)
     /// (7.2.13 INTERNAL AUTHENTICATE)
-    fn internal_authenticate(
+    pub fn internal_authenticate(
         &mut self,
         data: Vec<u8>,
     ) -> Result<Vec<u8>, Error> {
