a7731ec467597bc1d219fa6a130cd3d086a374ac
Nora Widdecke <nora@sequoia-pgp.org>
2022-10-25 13:38:08 +0200

    opgpcard: Add Nora to license headers

--- tools/src/bin/opgpcard/cli.rs
+++ tools/src/bin/opgpcard/cli.rs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2021-2022 Heiko Schaefer <heiko@schaefer.name>
+// SPDX-FileCopyrightText: 2022 Nora Widdecke <mail@nora.pink>
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use clap::{AppSettings, Parser, ValueEnum};
