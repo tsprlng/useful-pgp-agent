77ed66bde7158c96d71e14e6cbd96b9e3f9853ed
Nora Widdecke <nora@sequoia-pgp.org>
2022-10-26 18:59:55 +0200

    opgpcard: Add Lars to license header

--- tools/src/bin/opgpcard/main.rs
+++ tools/src/bin/opgpcard/main.rs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2021-2022 Heiko Schaefer <heiko@schaefer.name>
+// SPDX-FileCopyrightText: 2022 Lars Wirzenius <liw@liw.fi>
 // SPDX-FileCopyrightText: 2022 Nora Widdecke <mail@nora.pink>
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
