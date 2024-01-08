799d077d559f0184cd6f14a7a71ae0fba5f79c35
RyanSquared <ryan@hashbang.sh>
2024-01-07 21:51:21 -0500

    openpgp-card: optimize use of iter/collect to use to_vec

--- openpgp-card/src/lib.rs
+++ openpgp-card/src/lib.rs
@@ -1205,12 +1205,7 @@ impl<'a> Transaction<'a> {
         log::info!("OpenPgpTransaction: set_creation_time");
 
         // Timestamp update
-        let time_value: Vec<u8> = time
-            .get()
-            .to_be_bytes()
-            .iter()
-            .copied()
-            .collect();
+        let time_value: Vec<u8> = time.get().to_be_bytes().to_vec();
 
         let cmd = commands::put_data(key_type.timestamp_put_tag(), time_value);
 
