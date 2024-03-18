e2a34c556cbf739c427a43a65cb0f73c10415615
Heiko Schaefer <heiko@schaefer.name>
2024-03-18 14:46:04 +0100

    naming and error handling cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ use pgp::packet::PublicKey;
 use pgp::types::{KeyId, KeyTrait, Mpi, PublicKeyTrait, SecretKeyRepr, SecretKeyTrait};
 use rand::{CryptoRng, Rng};
 
-/// An individual OpenPGP card key slot
+/// An individual OpenPGP card key slot, which can be used for private key operations.
 pub struct CardSlot<'a> {
     tx: Mutex<Transaction<'a>>,
 
@@ -25,21 +25,21 @@ pub struct CardSlot<'a> {
     //
     // The distinction between primary and subkey is irrelevant here, but we have to use some type.
     // So we model the key data as a public primary key packet.
-    pubkey: PublicKey,
+    public_key: PublicKey,
 }
 
 impl<'a> CardSlot<'a> {
     /// Set up a CardSigner for the card behind `tx`, using the key slot for `key_type`.
     ///
-    /// Initializes the CardSigner based on public key information obtained from `pubkey`.
-    pub fn with_pubkey(
+    /// Initializes the CardSigner based on public key information obtained from `public_key`.
+    pub fn with_public_key(
         tx: Transaction<'a>,
         key_type: KeyType,
-        pubkey: PublicKey,
+        public_key: PublicKey,
     ) -> Result<Self, pgp::errors::Error> {
         Ok(Self {
             tx: Mutex::new(tx),
-            pubkey,
+            public_key,
             key_type,
         })
     }
@@ -53,14 +53,24 @@ impl<'a> CardSlot<'a> {
     ) -> Result<Self, pgp::errors::Error> {
         let pk = rpgp::pubkey_from_card(&mut tx, key_type)?;
 
-        Self::with_pubkey(tx, key_type, pk)
+        Self::with_public_key(tx, key_type, pk)
+    }
+
+    /// The OpenPGP public key material that corresponds to the key in this CardSlot
+    pub fn public_key(&self) -> &PublicKey {
+        &self.public_key
+    }
+
+    /// The card slot that this CardSlot uses
+    pub fn key_type(&self) -> KeyType {
+        self.key_type
     }
 }
 
 impl Debug for CardSlot<'_> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         // FIXME: also show card identifier
-        write!(f, "CardSigner for {:?}", self.pubkey)?;
+        write!(f, "CardSigner for {:?}", self.public_key)?;
 
         Ok(())
     }
@@ -68,15 +78,15 @@ impl Debug for CardSlot<'_> {
 
 impl KeyTrait for CardSlot<'_> {
     fn fingerprint(&self) -> Vec<u8> {
-        self.pubkey.fingerprint()
+        self.public_key.fingerprint()
     }
 
     fn key_id(&self) -> KeyId {
-        self.pubkey.key_id()
+        self.public_key.key_id()
     }
 
     fn algorithm(&self) -> PublicKeyAlgorithm {
-        self.pubkey.algorithm()
+        self.public_key.algorithm()
     }
 }
 
@@ -87,7 +97,7 @@ impl PublicKeyTrait for CardSlot<'_> {
         data: &[u8],
         sig: &[Mpi],
     ) -> pgp::errors::Result<()> {
-        self.pubkey.verify_signature(hash, data, sig)
+        self.public_key.verify_signature(hash, data, sig)
     }
 
     fn encrypt<R: CryptoRng + Rng>(
@@ -95,11 +105,11 @@ impl PublicKeyTrait for CardSlot<'_> {
         rng: &mut R,
         plain: &[u8],
     ) -> pgp::errors::Result<Vec<Mpi>> {
-        self.pubkey.encrypt(rng, plain)
+        self.public_key.encrypt(rng, plain)
     }
 
     fn to_writer_old(&self, writer: &mut impl std::io::Write) -> pgp::errors::Result<()> {
-        self.pubkey.to_writer_old(writer)
+        self.public_key.to_writer_old(writer)
     }
 }
 
@@ -115,7 +125,9 @@ impl SecretKeyTrait for CardSlot<'_> {
     {
         // FIXME: does this get called? if so, what happens to `work`?
 
-        unimplemented!();
+        Err(pgp::errors::Error::Unimplemented(
+            "CardSlot::unlock is not implemented".to_string(),
+        ))
     }
 
     fn create_signature<F>(
@@ -143,6 +155,6 @@ impl SecretKeyTrait for CardSlot<'_> {
     }
 
     fn public_key(&self) -> Self::PublicKey {
-        self.pubkey.clone()
+        self.public_key.clone()
     }
 }
--- src/rpgp.rs
+++ src/rpgp.rs
@@ -16,7 +16,7 @@ fn pubkey(
 ) -> Result<PublicKey, pgp::errors::Error> {
     PublicKey::new(
         Version::New,
-        KeyVersion::V4, // FIXME: don't hardwire?
+        KeyVersion::V4, // FIXME: handle other OpenPGP key versions, later
         algo,
         created,
         None,
@@ -24,22 +24,54 @@ fn pubkey(
     )
 }
 
+fn map_curve(c: &Curve) -> Result<ECCCurve, pgp::errors::Error> {
+    Ok(match c {
+        Curve::NistP256r1 => ECCCurve::P256,
+        Curve::NistP384r1 => ECCCurve::P384,
+        Curve::NistP521r1 => ECCCurve::P521,
+        Curve::BrainpoolP256r1 => ECCCurve::BrainpoolP256r1,
+        Curve::BrainpoolP384r1 => ECCCurve::BrainpoolP384r1,
+        Curve::BrainpoolP512r1 => ECCCurve::BrainpoolP512r1,
+        Curve::Ed25519 => ECCCurve::Ed25519,
+        Curve::Cv25519 => ECCCurve::Curve25519,
+
+        _ => {
+            return Err(pgp::errors::Error::Unimplemented(format!(
+                "Can't map curve {:?}",
+                c
+            )))
+        }
+    })
+}
+
+fn map_card_err(e: openpgp_card::Error) -> pgp::errors::Error {
+    pgp::errors::Error::Message(format!("openpgp_card error: {:?}", e))
+}
+
 pub(crate) fn pubkey_from_card(
     tx: &mut Transaction,
     key_type: KeyType,
 ) -> Result<PublicKey, pgp::errors::Error> {
-    let pk = tx.public_key(key_type).expect("FIXME");
+    let pk = tx.public_key(key_type).map_err(map_card_err)?;
 
-    let ard = tx.application_related_data().expect("FIXME");
-    let kgt = ard.key_generation_times().expect("FIXME");
-    let created = match key_type {
+    let ard = tx.application_related_data().map_err(map_card_err)?;
+    let kgt = ard.key_generation_times().map_err(map_card_err)?;
+
+    let Some(created) = (match key_type {
         KeyType::Signing => kgt.signature().cloned(),
         KeyType::Decryption => kgt.decryption().cloned(),
         KeyType::Authentication => kgt.authentication().cloned(),
-        KeyType::Attestation => ard.attestation_key_generation_time().expect("FIXME"),
-        _ => unimplemented!(), // FIXME
-    }
-    .expect("FIXME");
+        KeyType::Attestation => ard.attestation_key_generation_time().map_err(|e| {
+            pgp::errors::Error::Message(format!("Get attestation_key_generation_time: {:?}", e,))
+        })?,
+        _ => unimplemented!(), // FIXME: this openpgp-card type should be exhaustive
+    }) else {
+        // KeyGenerationTime is None
+        return Err(pgp::errors::Error::Message(format!(
+            "No creation time set for OpenPGP card key type {:?}",
+            key_type,
+        )));
+    };
 
     let created =
         DateTime::<Utc>::from_timestamp(created.get() as i64, 0).expect("u32 time from card");
@@ -56,20 +88,11 @@ pub(crate) fn pubkey_from_card(
 
         PublicKeyMaterial::E(ecc) => match ecc.algo() {
             AlgorithmAttributes::Ecc(ecc_attr) => {
-                let et = ecc_attr.ecc_type();
+                let typ = ecc_attr.ecc_type();
 
-                fn map_curve(c: &Curve) -> ECCCurve {
-                    match c {
-                        Curve::Ed25519 => ECCCurve::Ed25519,
-                        Curve::Cv25519 => ECCCurve::Curve25519,
+                let curve = map_curve(ecc_attr.curve())?;
 
-                        _ => unimplemented!("map_curve {:?}", c),
-                    }
-                }
-
-                let curve = map_curve(ecc_attr.curve());
-
-                let (pka, pp) = match et {
+                let (pka, pp) = match typ {
                     EccType::ECDH => (
                         PublicKeyAlgorithm::ECDH,
                         PublicParams::ECDH {
@@ -82,16 +105,18 @@ pub(crate) fn pubkey_from_card(
 
                     EccType::ECDSA => (
                         PublicKeyAlgorithm::ECDSA,
-                        PublicParams::ECDSA(
-                            EcdsaPublicParams::try_from_mpi(ecc.data().into(), curve)
-                                .expect("FIXME"),
-                        ),
+                        PublicParams::ECDSA(EcdsaPublicParams::try_from_mpi(
+                            ecc.data().into(),
+                            curve,
+                        )?),
                     ),
 
                     EccType::EdDSA => {
                         let mut q = ecc.data().to_vec();
                         if q.len() == 32 {
-                            q.insert(0, 0x40); // FIXME?
+                            // Add prefix to mark that this MPI uses EdDSA point representation.
+                            // See https://datatracker.ietf.org/doc/draft-koch-eddsa-for-openpgp/
+                            q.insert(0, 0x40);
                         }
 
                         (
@@ -100,13 +125,20 @@ pub(crate) fn pubkey_from_card(
                         )
                     }
 
-                    _ => unimplemented!(),
+                    _ => unimplemented!(), // FIXME: openpgp-card EccType type should be exhaustive
                 };
 
                 pubkey(pka, created, pp)
             }
-            _ => unimplemented!(),
+
+            _ => Err(pgp::errors::Error::Message(format!(
+                "Unexpected AlgorithmAttributes type in Ecc {:?}",
+                ecc.algo(),
+            ))),
         },
-        _ => unimplemented!(),
+        _ => Err(pgp::errors::Error::Message(format!(
+            "Unexpected PublicKeyMaterial type {:?}",
+            pk,
+        ))),
     }
 }
