c11ac3ee4c12e57e7531fe54e44d7bbcd0ef1ba2
Heiko Schaefer <heiko@schaefer.name>
2024-04-14 19:14:42 +0200

    normalize RSA signature Mpi

--- src/lib.rs
+++ src/lib.rs
@@ -217,7 +217,7 @@ impl SecretKeyTrait for CardSlot<'_, '_> {
         };
 
         let mpis = match self.public_key.algorithm() {
-            PublicKeyAlgorithm::RSA => vec![sig.into()],
+            PublicKeyAlgorithm::RSA => vec![Mpi::from_raw(sig)],
 
             PublicKeyAlgorithm::ECDSA => {
                 let mid = sig.len() / 2;
